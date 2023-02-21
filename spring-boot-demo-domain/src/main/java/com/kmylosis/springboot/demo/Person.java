79c73296963dbb9ec269ce281760b268ec0c43e0
kmylosis <31594417+kmylosis@users.noreply.github.com>
2023-02-21 23:48:06 +0200

    add lombok

---
+++ spring-boot-demo-domain/src/main/java/com/kmylosis/springboot/demo/Person.java
@@ -0,0 +1,20 @@
+package com.kmylosis.springboot.demo;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Person {
+
+  private String firstName;
+
+  private String lastName;
+
+  private LocalDate dateOfBirth;
+
+  private BigInteger identification;
+
+}
