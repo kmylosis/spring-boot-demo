4a940c775ca750faaaa6c8e0cfe0edb6cbcb97a7
kmylosis <31594417+kmylosis@users.noreply.github.com>
2023-02-24 00:39:14 +0200

    person config

--- spring-boot-demo-app/src/main/java/com/kmylosis/springboot/SpringBootDemoBasePackage.java
+++ spring-boot-demo-app/src/main/java/com/kmylosis/springboot/SpringBootDemoBasePackage.java
@@ -1,5 +1,4 @@
 package com.kmylosis.springboot;
 
 public interface SpringBootDemoBasePackage {
-
 }
---
+++ spring-boot-demo-core/src/main/java/com/kmylosis/springboot/demo/core/port/PersonConfig.java
@@ -0,0 +1,9 @@
+package com.kmylosis.springboot.demo.core.port;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kmylosis.springboot.demo.domain.core")
+public class PersonConfig {
+}
