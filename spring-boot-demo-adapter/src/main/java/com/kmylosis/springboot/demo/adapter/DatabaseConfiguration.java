93c8a058e8093ebcf6e2f369f0ed2db08f92997a
kmylosis <31594417+kmylosis@users.noreply.github.com>
2023-02-24 00:35:00 +0200

    add configuration and properties

--- spring-boot-demo-adapter/src/main/java/com/kmylosis/springboot/demo/adapter/DatabaseConfiguration.java
+++ spring-boot-demo-adapter/src/main/java/com/kmylosis/springboot/demo/adapter/DatabaseConfiguration.java
@@ -1,7 +1,11 @@
 package com.kmylosis.springboot.demo.adapter;
 
 import com.kmylosis.springboot.demo.adapter.outbound.repository.SpringBootDemoJPABasePackage;
+import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,5 +15,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackageClasses = SpringBootDemoJPABasePackage.class)
 @EnableTransactionManagement
 public class DatabaseConfiguration {
+
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource springBootDemoDataSource() {
+    return DataSourceBuilder.create().build();
+  }
 
 }
--- spring-boot-demo-app/src/main/java/com/kmylosis/springboot/app/Application.java
+++ spring-boot-demo-app/src/main/java/com/kmylosis/springboot/app/Application.java
@@ -1,39 +1,15 @@
 package com.kmylosis.springboot.app;
 
 import com.kmylosis.springboot.SpringBootDemoBasePackage;
-import com.kmylosis.springboot.demo.adapter.dto.PersonDTO;
-import com.kmylosis.springboot.demo.adapter.mappers.PersonMapper;
-import com.kmylosis.springboot.demo.domain.core.port.outbound.PersonRepository;
-import java.math.BigInteger;
-import java.time.LocalDate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackageClasses = {SpringBootDemoBasePackage.class})
 public class Application {
 
-  @Autowired
-  private static PersonRepository personRepository;
-
-  @Autowired
-  private static PersonMapper personMapper;
-
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
     System.out.println("Hello world!");
-    PersonDTO personDTO = PersonDTO.builder().withFirstName("Konstantinos").withLastName("Mylosis").withDateOfBirth(LocalDate.now())
-            .withIdentification(BigInteger.valueOf(123456)).build();
-    personRepository.save(personMapper.dtoToDomain(personDTO));
-
-    personRepository.findByIdentification(BigInteger.valueOf(123456));
   }
 
-  public static void setPersonMapper(PersonMapper personMapper) {
-    Application.personMapper = personMapper;
-  }
-
-  public static void setPersonRepository(PersonRepository personRepository) {
-    Application.personRepository = personRepository;
-  }
 }
